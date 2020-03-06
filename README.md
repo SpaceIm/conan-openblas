c002a6cbd2d5189e3681d8c3ba2cbc772cd41b62
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-03-06 10:43:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# conan-openblas
+
+## Package Status
+
+| Windows | Linux | macOS |
+|:-------:|:-----:|:-----:|
+|[![Build status](https://ci.appveyor.com/api/projects/status/k8t4t50xv2ukjle1/branch/testing%2F0.3.7?svg=true)](https://ci.appveyor.com/project/SpaceIm/conan-openblas)|![.github/workflows/conan.yml](https://github.com/SpaceIm/conan-openblas/workflows/.github/workflows/conan.yml/badge.svg?branch=testing%2F0.3.7)|[![Build Status](https://travis-ci.com/SpaceIm/conan-openblas.svg?branch=testing%2F0.3.7)](https://travis-ci.com/SpaceIm/conan-openblas)|
+
+## Description
+
+[Conan.io](https://conan.io) package for [OpenBLAS](https://github.com/xianyi/OpenBLAS).
---
+++ conanfile.py
@@ -0,0 +1,93 @@
+import os
+
+from conans import ConanFile, CMake, tools
+
+class OpenblasConan(ConanFile):
+    name = "openblas"
+    description = "An optimized BLAS library based on GotoBLAS2 1.13 BSD version"
+    license = "BSD-3-Clause"
+    topics = ("openblas", "blas", "lapack")
+    homepage = "https://www.openblas.net"
+    url = "https://github.com/conan-io/conan-center-index"
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "build_lapack": [True, False],
+        "use_thread": [True, False],
+        "dynamic_arch": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "build_lapack": False,
+        "use_thread": True,
+        "dynamic_arch": False
+    }
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename('OpenBLAS-{}'.format(self.version), self._source_subfolder)
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        if self.options.build_lapack:
+            self.output.warn("Building with lapack support requires a Fortran compiler.")
+
+        self._cmake.definitions["NOFORTRAN"] = not self.options.build_lapack
+        self._cmake.definitions["BUILD_WITHOUT_LAPACK"] = not self.options.build_lapack
+        self._cmake.definitions["DYNAMIC_ARCH"] = self.options.dynamic_arch
+        self._cmake.definitions["USE_THREAD"] = self.options.use_thread
+
+        # Required for safe concurrent calls to OpenBLAS routines
+        self._cmake.definitions["USE_LOCKING"] = not self.options.use_thread
+
+        self._cmake.definitions["MSVC_STATIC_CRT"] = False # don't, may lie to consumer, /MD or /MT is managed by conan
+
+        # This is a workaround to add the libm dependency on linux,
+        # which is required to successfully compile on older gcc versions.
+        self._cmake.definitions["ANDROID"] = self.settings.os in ["Linux", "Android"]
+
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        tools.rmdir(os.path.join(self.package_folder, "share"))
+
+    def package_info(self):
+        self.env_info.OpenBLAS_HOME = self.package_folder
+        self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            if self.options.use_thread:
+                self.cpp_info.system_libs.append("pthread")
+            if self.options.build_lapack:
+                self.cpp_info.system_libs.append("gfortran")
+        self.cpp_info.names["cmake_find_package"] = "OpenBLAS"
+        self.cpp_info.names["cmake_find_package_multi"] = "OpenBLAS"
+        self.cpp_info.names['pkg_config'] = "OpenBLAS"
